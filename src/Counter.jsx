4528cfb7cc14e5a1d22318d167585e1b55d4f3db
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-06-17 22:57:55 +0530

    styled counter app

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Counter = () => {
   return (
-    <div>
-        <div>Increment && Decrement</div>
-        <div>
-            <div>-</div>
-            <div>0</div>
-            <div>+</div>
+    <div className=' bg-[#2d2d2d] h-[100vh] flex justify-center items-center flex-col'>
+        <div className='font-medium text-purple-700 text-2xl mb-3'> Increment && Decrement</div>
+        <div className='flex bg-white w-[250px] gap-3 justify-center py-3 items-center '>
+            <button className='text-3xl font-medium pl-4 mx-auto '>-</button>
+            <div className='text-2xl border-x-2 border-black px-12 '>0</div>
+            <button className='text-3xl  font-medium pr-4 mx-auto'>+</button>
         </div>
-        
+        <div>Reset</div>
     </div>
   )
 }
