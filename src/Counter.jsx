35517dd36caef8b6ebc1f70332f586751cc11672
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-06-17 22:24:04 +0530

    Creating a counter component

---
+++ src/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div>
+        <div>Increment && Decrement</div>
+        <div>
+            <div>-</div>
+            <div>0</div>
+            <div>+</div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Counter
