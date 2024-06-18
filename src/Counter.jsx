20b8e6917aa2f70abed19f8a7bb6aabb9ee897c3
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-06-18 22:25:40 +0530

    Added functionality

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Counter = () => {
+  const [counter,setCounter]=useState(0);
+  function incrementHandler(){
+    setCounter(counter+1)
+  }
+  function decrementHandler(){
+    setCounter(counter-1)
+  }
+  
   return (
     <div className=' bg-[#2d2d2d] h-[100vh] flex justify-center items-center flex-col'>
         <div className='font-medium text-purple-700 text-2xl mb-3'> Increment && Decrement</div>
-        <div className='flex bg-white w-[250px] gap-3 justify-center py-3 items-center '>
-            <button className='text-3xl font-medium pl-4 mx-auto '>-</button>
-            <div className='text-2xl border-x-2 border-black px-12 '>0</div>
-            <button className='text-3xl  font-medium pr-4 mx-auto'>+</button>
+        <div className='flex bg-white w-[250px] gap-3 justify-center  items-center align-top '>
+            <button className='text-3xl font-medium px-4 py-2 flex items-center justify-center' onClick={decrementHandler}>-</button>
+            <div className='text-2xl border-x-2 border-black w-[100px] '>{counter}</div>
+            <button className='text-3xl  font-medium  px-4 flex items-center justify-center'  onClick={incrementHandler}>+</button>
         </div>
-        <div>Reset</div>
+        <button className='text-white font-semibold mt-2  border-black rounded-sm px-2 bg-purple-700' onClick={()=>{setCounter(0)}}>Reset</button>
     </div>
   )
 }
