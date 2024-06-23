66296e4ad05a4e1c1664bbdee33fad573b481137
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-06-23 22:49:02 +0530

    extending the app by adding todo tool

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from "./Counter"
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./Components/Counter";
+import Todo from "./Components/Todo";
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState(null);
+
   return (
     <div className="App">
       {/* <h1 className='font-bold text-3xl text-purple-800 '>Hey its tailwind baby </h1> */}
-      <Counter></Counter>
+      <button
+        className="border bg-[#4f4f4f] px-4 py-2 text-2xl text-white font-semibold rounded-md "
+        onClick={() => setActiveComponent("Counter")}
+      >
+        Counter
+      </button>
+      <button
+        className="border bg-[#4f4f4f] px-4 py-2 text-2xl text-white font-semibold rounded-md"
+        onClick={() => setActiveComponent("Todo")}
+      >
+        ToDo
+      </button>
+
+      <div className="component-container">
+        {activeComponent === "Counter" && <Counter />}
+        {activeComponent === "Todo" && <Todo />}
+      </div>
     </div>
   );
 }
